d0a7e638f9ccc11253db6a519cd05ccd972143e3
Nadufa <nadufa18404@gmail.com>
2024-02-22 14:53:07 +0300

    add 5 kyu solution

--- one.js
+++ one.js
@@ -1,15 +1,14 @@
 // console.log('1')
+
+// 6 kyu
+
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
 //
+//     Examples:
 //
-// // 6 kyu
-//
-// // Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
-// //
-// //     Examples:
-// //
-// // * 'abc' =>  ['ab', 'c_']
-// // * 'abcdef' => ['ab', 'cd', 'ef']
-//
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
 // function solution(str){
 //     const arr = str.split('')
 //     let result = []
@@ -25,8 +24,6 @@
 
 
 // 6 kyu
-
-
 // Given an array of integers, find the one that appears an odd number of times.
 //
 //     There will always be only one integer that appears an odd number of times.
@@ -59,18 +56,52 @@
 // console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]))
 
 
-
 // 7 kyu
-
 // You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
 //
 //     You can assume all values in the array are numbers.
 
-function smallEnough(arr, limit) {
-    return  arr.filter(el => el <= limit).length === arr.length
+// function smallEnough(arr, limit) {
+//     return  arr.filter(el => el <= limit).length === arr.length
+// }
+//
+// console.log(smallEnough([66, 101], 200))
+// console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100))
+// console.log(smallEnough([101, 45, 75, 105, 99, 107], 107))
+// console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120))
+
+
+//5 kyu
+
+// Write a function named first_non_repeating_letter† that takes a string input, and returns the first character that is not repeated anywhere in the string.
+//
+//     For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+//
+//     As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+//
+//     If a string contains all repeating characters, it should return an empty string ("");
+//
+// † Note: the function is called firstNonRepeatingLetter for historical reasons, but your function should handle any Unicode character.
+
+function firstNonRepeatingLetter(str) {
+    let arr = str.toLowerCase().split('')
+    for (let i = 0; i < arr.length; i++) {
+        let isNotRepeat = true
+        for (let j = 0; j < arr.length; j++) {
+            if (i !== j && arr[i] === arr[j]) {
+                isNotRepeat = false
+                j = arr.length
+            }
+        }
+        if (isNotRepeat) {
+            return str[i]
+        }
+    }
+    return ''
 }
 
-console.log(smallEnough([66, 101], 200))
-console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100))
-console.log(smallEnough([101, 45, 75, 105, 99, 107], 107))
-console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120))
+console.log(firstNonRepeatingLetter('a'))
+console.log(firstNonRepeatingLetter('stress'))
+console.log(firstNonRepeatingLetter('moonmen'))
+console.log(firstNonRepeatingLetter('abcabc'))
+
