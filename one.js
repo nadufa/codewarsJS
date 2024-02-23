7c4241575d1eda3308f7049b145ff2f3b4aeaf17
Nadufa <nadufa18404@gmail.com>
2024-02-23 22:13:37 +0300

    add 6 kyu solution

--- one.js
+++ one.js
@@ -152,28 +152,60 @@
 //
 //     [[1,2,3],[2,4,6],[3,6,9]]
 
-multiplicationTable = function (size) {
-    let arr = []
-    for (let i = 0; i < size; i++) {
-        arr.push([])
-        if (i === 0) {
-            for (let j = 0; j < size; j++) {
-                arr[i].push(j + 1)
+// multiplicationTable = function (size) {
+//     let arr = []
+//     for (let i = 0; i < size; i++) {
+//         arr.push([])
+//         if (i === 0) {
+//             for (let j = 0; j < size; j++) {
+//                 arr[i].push(j + 1)
+//             }
+//         } else {
+//             arr[i].push(i+1)
+//         }
+//     }
+//     for (let i = 1; i < size; i++) {
+//         for (let j = 1; j < size; j++) {
+//             arr[i] = [...arr[i], arr[0][j] * arr[i][0]]
+//         }
+//     }
+//     return arr
+// }
+//
+//
+// console.log(multiplicationTable(1))
+// console.log(multiplicationTable(2))
+// console.log(multiplicationTable(3))
+// console.log(multiplicationTable(5))
+
+
+
+// 6 kyu
+
+// You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+
+function findEvenIndex(arr)
+{
+    for (let i = 0; i < arr.length; i++) {
+        let sumBefore = 0
+        let sumAfter = 0
+        for (let j = 0; j < arr.length; j++) {
+            if (j < i) {
+                sumBefore += arr[j]
             }
-        } else {
-            arr[i].push(i+1)
+            if (j > i) {
+                sumAfter += arr[j]
+            }
+        }
+        if (sumBefore === sumAfter ) {
+            return i
         }
     }
-    for (let i = 1; i < size; i++) {
-        for (let j = 1; j < size; j++) {
-            arr[i] = [...arr[i], arr[0][j] * arr[i][0]]
-        }
-    }
-    return arr
+    return -1
 }
 
-
-console.log(multiplicationTable(1))
-console.log(multiplicationTable(2))
-console.log(multiplicationTable(3))
-console.log(multiplicationTable(5))
+console.log(findEvenIndex([1,2,3,4,3,2,1]))
+console.log(findEvenIndex([1,100,50,-51,1,1]))
+console.log(findEvenIndex([1,2,3,4,5,6]))
+console.log(findEvenIndex([20,10,30,10,10,15,35]))
+console.log(findEvenIndex([20,10,-80,10,10,15,35]))
